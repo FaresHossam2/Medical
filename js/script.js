35f72774d73129ba28cfcf1998e7cf15a231321b
fareshossam2 <fareshossam201@gmail.com>
2024-02-16 01:55:09 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    
+    $("#bar").click(function() {
+       
+        $("nav ul").css("right", " -100px");
+       
+    });
+    $("#close").click(function() {
+       
+        $("nav ul").css("right", " -3000px");
+
+    });
+
+    });
